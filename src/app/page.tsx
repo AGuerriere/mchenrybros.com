3daa4a40571974532df54909d69248cf850406a3
Antonio Guerriere <antonioguerriere@gmail.com>
2024-01-26 18:16:16 +0000

    coming soon placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,15 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center bg-emerald-600 w-full h-[100vh]">
+      <div className="flex flex-col justify-center items-center bg-emerald-600 w-full h-[100vh] p-10">
         <Image
           src="/images/logo-white.png"
           width={500}
           height={500}
           alt="McHenry Bros Logo"
         />
-        <h1 className="text-white text-[40px] mt-20">McHenry Bros</h1>
-        <p className="text-white text-[40px] mt-20">Coming Soon!</p>
-        <p className="text-white text-[40px] mt-20">For enquiries call Darren +44 7751 116624</p>
+        <p className="text-white text-[18px] xl:text-[40px] mt-20">Website Coming Soon!</p>
+        <p className="text-white text-[18px] xl:text-[40px] mt-20">For enquiries call: <br /> Darren: +44 7751 116624 <br />Jonathan: +44 7763 071143</p>
       </div>
     </>
   );
