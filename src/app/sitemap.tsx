aa9981c20cbb688abb5d4d48ebb1a7e6131d6df4
Antonio Guerriere <antonioguerriere@gmail.com>
2024-02-16 23:41:20 +0000

    updated sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -32,6 +32,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
+    {
+      url: 'https://mchenrybros.com/ballycasle-homecare',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
     
   ]
 }
